7d150d68d46586bc127444153d5aaf8640713e6f
Bethany <bethany.grogg@gmail.com>
2019-06-02 12:55:59 -0400

    now adding to log.txt

--- liri.js
+++ liri.js
@@ -15,8 +15,32 @@ var fs = require('fs');
 var request = process.argv[2];
 var name = process.argv[3];
 
+function doWhat(request, name){
+	switch(request){
+    
+    case "concert-this":
+	    doConcert(name);
+    break;
+    
+	case "spotify-this-song":
+		doSpotify(name);
+	break;
+
+	case "movie-this":
+		doMovie(name);
+	break;
+
+	case "do-what-it-says":
+		doRandom();
+	break;
+
+	default:
+	break;
+	}
+}
+
 //This section is for getting concert information
-if (request == 'concert-this') {
+function doConcert(name) {
     var queryUrl = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
 
     axios.get(queryUrl).then(
@@ -25,12 +49,16 @@ if (request == 'concert-this') {
            console.log("Venue Name: " + response[0].venue.name +
            "\nVenue location: " + response[0].venue.city +
            "\nEvent Date: " + response[0].datetime);
+
+           appendFile("Venue Name: " + response[0].venue.name +
+           "\nVenue location: " + response[0].venue.city +
+           "\nEvent Date: " + response[0].datetime);
     });
 
 }
 
 //This section is for getting song information
-if (request == 'spotify-this-song') {
+function doSpotify(name) {
 
     if (name == null) {
         name = 'The Sign';
@@ -53,17 +81,25 @@ if (request == 'spotify-this-song') {
                 "\nLink to Song: " + song.preview_url +
                 //The album that the song is from
                 "\nAlbum Name: " + song.album.name);
+
+                appendFile("Artist: " + song.artists[0].name +
+                //The song's name
+                "\nSong Name: " + song.name +
+                //A preview link of the song
+                "\nLink to Song: " + song.preview_url +
+                //The album that the song is from
+                "\nAlbum Name: " + song.album.name);
             }
         };  
 
      
       });
 
-}
+};
 
 
 //This section is for getting movie information
-if (request == 'movie-this') {
+function doMovie(name) {
 
     if (name == null) {
         name = 'Mr. Nobody';
@@ -81,14 +117,44 @@ if (request == 'movie-this') {
             "\nMovie Language: " + response.data.Language +
             "\nMovie Plot: " + response.data.Plot +
             "\nActors: " + response.data.Actors);
-        });
+       
+
+        appendFile("Movie Name: " + response.data.Title +
+        "\nYear: " + response.data.Year +
+        "\nOMDB Rating: " + response.data.imdbRating +
+        "\nRotten Tomatoes Rating: " + response.data.Ratings +
+        "\nCountry it was produced: " + response.data.Country +
+        "\nMovie Language: " + response.data.Language +
+        "\nMovie Plot: " + response.data.Plot +
+        "\nActors: " + response.data.Actors);
+    });
 }
 
 //This section is for do-what-it-says
-if (request == 'do-what-it-says') {
+function doRandom() {
 
     fs.readFile("random.txt", 'utf8', function(err, data){
         console.log(data);
+
+        var dataArray = data.split(',');
+        request = dataArray[0];
+        name = dataArray[1];
+        doWhat(request, name);
+
 })
 
 };
+
+function appendFile(data){
+
+	//Output all that happens into a log.txt file
+	fs.appendFile("log.txt", data , function(err){
+
+		//If an error happens while trying to write to the file
+		if (err){
+			return console.log(err);
+		}
+	});
+}
+
+doWhat(request, name);
